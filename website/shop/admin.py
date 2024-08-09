f5b79f132b22a32b911dd245c7a84be555e2a488
saurabh <saurabhsinghal579@gmail.com>
2024-08-09 16:33:29 +0530

    Complete project

--- website/shop/admin.py
+++ website/shop/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 
-from .models import Products
+from .models import Products,Contact,Order,OrderUpdate
 admin.site.register(Products)
+admin.site.register(Contact)
+admin.site.register(Order)
+admin.site.register(OrderUpdate)
